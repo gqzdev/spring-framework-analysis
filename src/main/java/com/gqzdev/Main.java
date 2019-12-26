c07d0c019a132e201772998e41638c71f4895d74
gqzdev <gqzdev@gmail.com>
2019-12-27 00:57:46 +0800

    坚持,结果不会差的

---
+++ src/main/java/com/gqzdev/Main.java
@@ -0,0 +1,14 @@
+package com.gqzdev;
+
+/**
+ * @ClassName Main
+ * @Description test构建Spring源码是否成功
+ * @Author ganquanzhong
+ * @Date2019/12/27 0:53
+ * @Version
+ **/
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Hello ganquanzhong,吃透Spring源码");
+	}
+}
