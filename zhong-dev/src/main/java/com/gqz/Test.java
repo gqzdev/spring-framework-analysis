59cbfb5b92762bb5be2ef841c5e87e5879e6f24d
quanzhong <ganquanzhong96@gmail.com>
2020-04-02 11:38:16 +0800

    第三方组件

---
+++ zhong-dev/src/main/java/com/gqz/Test.java
@@ -0,0 +1,17 @@
+package com.gqz;
+
+/**
+ * @ClassName: Test
+ * @author: ganquanzhong
+ * @date: 2020/4/2 11:32
+ */
+public class Test {
+
+
+
+	public static  void  main(String[] args){
+
+		System.out.println("整合其他第三方组件！");
+
+	}
+}
